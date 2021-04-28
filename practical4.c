3326227411a6d196118503f401b83e40818f7ef2
jeelpatel231 <jeelpatel231@gmail.com>
2021-04-28 17:24:54 +0530

    push prac 4

---
+++ practical4.c
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2021 Jeel 
+This code is made by @jeelpatel231 on github and you shall only use this code under the compliance of license
+*/
+
+#include <stdio.h>
+
+int main(){
+    char str[2];
+    scanf("%s", str);
+    printf("Input: %c%c \n", str[0], str[1]); 
+    printf("output: %c%c \n", str[1], str[0]);
+    }
