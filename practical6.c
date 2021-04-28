919cc804bbe48cf0dab6036bd727d222d10b564b
jeelpatel231 <jeelpatel231@gmail.com>
2021-04-28 17:35:18 +0530

    push prac 6

---
+++ practical6.c
@@ -0,0 +1,16 @@
+
+/*
+Copyright (c) 2021 Jeel 
+This code is made by @jeelpatel231 on github and you shall only use this code under the compliance of license
+*/
+
+// convert kilometers to other units
+
+#include <stdio.h>
+
+int main(){
+    float a;
+    printf("insert temperature in centigrade : ");
+    scanf("%f", &a);
+    printf("Distance in Meters : %f \n" , 1.8*a +32);
+    }
