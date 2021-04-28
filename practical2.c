fa71800eb2b3ce2c8715e2b18d1d88214661642a
jeelpatel231 <jeelpatel231@gmail.com>
2021-04-28 16:48:23 +0530

    practical 2 push

---
+++ practical2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float h,b;
+    printf("insert height : h =");
+    scanf("%f", &h);
+    printf("insert base : b =");
+    scanf("%f", &b);
+    printf("value of area = %f", h*b*0.5);
+}
